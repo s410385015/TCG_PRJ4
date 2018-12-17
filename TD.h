c9b62be5c8feae8993462d1f5dd6c1eccc80d97b
GoWuG4 <B10102017@mail.ntust.edu.tw>
2018-12-17 09:58:23 +0800

    init

---
+++ TD.h
@@ -0,0 +1,76 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include<vector>
+#include <fstream>
+#include"weight.h"
+#include"board.h"
+
+
+
+class TD{
+    private:
+        std::vector<weight*> weights;
+    public:
+        TD(){}
+        ~TD(){}
+    
+        void add(weight* w){
+            weights.push_back(w);
+        }
+
+        float estimate(const board& b)const{
+            float value=0;
+            
+            for(weight* w: weights)
+                value+=w->estimate(b);
+            
+            return value;
+        }
+
+        float update(const board& b,float u) const
+        {
+            float a=u/weights.size();
+            float value=0;
+            for(weight* w:weights)
+                value+=w->update(b,a);
+            
+            return value;
+        }
+
+   
+
+        void UpdateEpisode(std::vector<state>& path,float alpha=0.01)const
+	    {
+    
+            float exact=0;
+            for (path.pop_back(); path.size(); path.pop_back()) {
+                state& move = path.back();
+                float error = exact - (move.GetValue() - move.GetReward());
+                exact = move.GetReward()+update(move.GetAfter(),alpha*error);
+		    }
+	    }
+
+        void Save()
+        {
+            std::string name[4]={"w1.txt","w2.txt","w3.txt","w4.txt"};
+            for(int i=0;i<(int)weights.size();i++)
+            {
+                std::ofstream out(name[i], std::ios::out | std::ios::binary | std::ios::trunc);
+                out<<(*weights[i]);
+            }
+        }
+
+        void Load()
+        {
+            std::string name[4]={"w1.txt","w2.txt","w3.txt","w4.txt"};
+            int i=0;
+            for(weight* w:weights)
+            {
+                std::ifstream in;
+		        in.open(name[i++], std::ios::in | std::ios::binary);
+                in>>*w;
+                in.close();
+            }
+        }
+};
---
+++ agent.h
@@ -0,0 +1,234 @@
+#pragma once
+#include <string>
+#include <random>
+#include <sstream>
+#include <map>
+#include <type_traits>
+#include <algorithm>
+#include <deque>
+#include "board.h"
+#include "action.h"
+#include "TD.h"
+#include "weight.h"
+class agent {
+public:
+	agent(const std::string& args = "") {
+		std::stringstream ss("name=unknown role=unknown " + args);
+		for (std::string pair; ss >> pair; ) {
+			std::string key = pair.substr(0, pair.find('='));
+			std::string value = pair.substr(pair.find('=') + 1);
+			meta[key] = { value };
+		}
+	}
+	virtual ~agent() {}
+	virtual void open_episode(const std::string& flag = "") {}
+	virtual void close_episode(const std::string& flag = "") {}
+	virtual action take_action(const board& b) { return action(); }
+	virtual bool check_for_win(const board& b) { return false; }
+
+public:
+	virtual std::string property(const std::string& key) const { return meta.at(key); }
+	virtual void notify(const std::string& msg) { meta[msg.substr(0, msg.find('='))] = { msg.substr(msg.find('=') + 1) }; }
+	virtual std::string name() const { return property("name"); }
+	virtual std::string role() const { return property("role"); }
+
+protected:
+	typedef std::string key;
+	struct value {
+		std::string value;
+		operator std::string() const { return value; }
+		template<typename numeric, typename = typename std::enable_if<std::is_arithmetic<numeric>::value, numeric>::type>
+		operator numeric() const { return numeric(std::stod(value)); }
+	};
+	std::map<key, value> meta;
+};
+
+class random_agent : public agent {
+public:
+	random_agent(const std::string& args = "") : agent(args) {
+		if (meta.find("seed") != meta.end())
+			engine.seed(int(meta["seed"]));
+	}
+	virtual ~random_agent() {}
+
+protected:
+	std::default_random_engine engine;
+};
+
+/**
+ * random environment
+ * add a new random tile to an empty cell
+ * 2-tile: 90%
+ * 4-tile: 10%
+ */
+class rndenv : public random_agent {
+public:
+	rndenv(const std::string& args = "") : random_agent("name=random role=environment " + args),
+		space({ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }), 
+		space_each_op({12,13,14,15,0,4,8,12,0,1,2,3,3,7,11,15})
+		{}
+
+	virtual action take_action(const board& after) {
+		
+		std::shuffle(space.begin(), space.end(), engine);
+		if(bags.size() <2) FillIn();
+		
+		board::cell tile = bags.front();
+		bags.pop_front();
+		int last_move=after.getLastMove();
+		//std::cout<<bags.size()<<" "<<bags.back()<<std::endl;
+
+		if(last_move==-1){
+			for (int pos : space) {
+				if (after(pos) != 0) continue;	
+						
+				return action::place(pos, tile,bags.front());
+			}
+		}else
+		{
+			std::shuffle(space_each_op[last_move].begin(), space_each_op[last_move].end(), engine);
+			for(int pos : space_each_op[last_move])
+			{
+				if (after(pos) != 0) continue;			
+				return action::place(pos, tile,bags.front());
+			}
+		}
+		return action();
+	}
+	void FillIn()
+	{	
+		std::array<int,3> tmp{1,2,3};
+		std::shuffle(tmp.begin(),tmp.end(),engine);
+		for(int i=0;i<3;i++)
+			bags.push_back(tmp[i]);
+			
+	}
+	void ResetBags()
+	{
+		bags.clear();
+		FillIn();
+	}
+private:
+	std::array<int, 16> space;
+	std::array<std::array<int,4>,4> space_each_op;
+	std::deque<int> bags;
+	
+};
+
+/**
+ * dummy player
+ * select a legal action randomly
+ */
+class player : public random_agent {
+public:
+	player(const std::string& args = "") : random_agent("name=dummy role=player " + args),
+		opcode({  0,1, 2, 3 }),
+		space_each_op({12,13,14,15,0,4,8,12,0,1,2,3,3,7,11,15}) {
+			path.reserve(20000);
+			td.add(new weight(1<<6*sizeof(float),{0, 1, 2, 3, 4, 5}));
+			td.add(new weight(1<<6*sizeof(float),{4, 5, 6, 7, 8, 9}));
+			td.add(new weight(1<<6*sizeof(float),{0, 1, 2, 4, 5, 6 }));
+			td.add(new weight(1<<6*sizeof(float),{4, 5, 6, 8, 9, 10 }));
+		}
+
+	virtual action take_action(const board& before) {
+
+		return SelectMove(before);
+		//std::shuffle(opcode.begin(), opcode.end(), engine);
+		/*
+		board:: reward _max=-1;
+		action act=action();
+		for (int op : opcode) {
+			board::reward reward = board(before).slide(op);
+			
+			if (reward == -1)
+				continue;
+			
+			for(int pos : space_each_op[op])
+			{
+				board after_slide(before);
+				after_slide.slide(op);
+				if(after_slide(pos)!=0) 
+					continue;
+
+				
+				board after(after_slide);
+
+				after.place(pos,before.getNextTile(),1);
+
+				for (int op1 : opcode) {
+					
+					
+					board::reward extra = board(after).slide(op1);
+					if (extra == -1)
+						continue;
+					reward+=extra;
+					//std::cout<<op<<" "<<op1<<" "<<reward<<" "<<reward1<<std::endl;
+					
+				}
+				
+			}
+			
+			if (reward >_max){ 
+						//return action::slide(op);
+				_max=reward;
+				act=action::slide(op);
+			}
+			
+		}
+		
+
+		*/
+		//return act;
+	}
+
+	action SelectMove(const board& before)
+	{
+		board:: reward _max=-1;
+		action act=action();
+		
+		state move;
+
+		move.SetBefore(before);
+	
+		for (int op : opcode) {
+			board after(before);
+			board::reward reward = after.slide(op);
+			if (reward == -1)
+				continue;
+
+			board:: reward new_reward=reward+td.estimate(after);
+			if(new_reward>_max)
+			{
+				_max=new_reward;
+				act=action::slide(op);
+				move.SetAfter(after);
+				move.SetReward(reward);
+				move.SetValue(new_reward);
+			}
+		}
+		
+		path.emplace_back(move);
+		return act;
+	}
+
+	void TD_update()
+	{
+		td.UpdateEpisode(path);
+		
+	}
+	
+	void TD_save()
+	{
+		td.Save();
+	}
+	void TD_load()
+	{
+		td.Load();
+	}
+private:
+	std::array<int, 4> opcode;
+	std::array<std::array<int,4>,4> space_each_op;
+	std::vector<state> path;
+	TD td;
+};
